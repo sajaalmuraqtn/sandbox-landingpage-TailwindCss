c0db1771e5106f210845202086b138c887063cfd
sajaalmuraqtn <sajaalmuraqtn@gmail.com>
2025-01-13 04:51:43 +0300

    nav and header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  theme: {
-   fontFamily:{
-     Inter:["Inter", 'serif']
-    },
+  theme: { 
     extend: { 
       screens:{
         '2md':'960px'
       },
       colors:{
-      primary:'#7E3AF2',
-      mainText:'#111928',
-      secondaryText:'#6B7280',
+       mainText:'#343F52',
+      secondaryText:'#60697B' 
 
     }},
   },
